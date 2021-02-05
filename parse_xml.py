5b1cd39ef23eee12ab0270aad7f547e309ede635
jxwleong <jason.xie.wei.leong@gmail.com>
2021-02-05 20:21:13 +0800

    Add function to remove file

--- parse_xml.py
+++ parse_xml.py
@@ -1,7 +1,12 @@
 import os
+import warnings
+
 import xml.etree.ElementTree as ET
 
+ROOT_DIR = os.path.join(os.getcwd())
 
+print("Root DIR")
+print(ROOT_DIR)
 tree = ET.parse('sample.xml')
 root = tree.getroot()
 ET.dump(tree)
@@ -19,4 +24,13 @@ def export_xml(tree, path):
     with open (path, 'wb') as f:
         tree.write(f)
 
-export_xml(tree, os.path.join(os.getcwd(), 'writer.xml'))
+#export_xml(tree, os.path.join(ROOT_DIR, 'writer.xml'))
+
+def delete_file(path):
+    if os.path.exists(path):
+        os.remove(path)
+    else:
+        warnings.warn('Invalid file path!', Warning)
+#delete_file(os.path.join(ROOT_DIR, 'Edited.xml'))
+
+delete_file('C:/ABC')
