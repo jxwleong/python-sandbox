be7a5fb1582669b641ec93a1da4ed76018535b11
jxwleong <jason.xie.wei.leong@gmail.com>
2021-02-05 19:58:48 +0800

    Added function to export xml file

--- parse_xml.py
+++ parse_xml.py
@@ -1,9 +1,10 @@
+import os
 import xml.etree.ElementTree as ET
 
 
 tree = ET.parse('sample.xml')
 root = tree.getroot()
-
+ET.dump(tree)
 for child in root:
     print(child.tag, child.attrib)
 
@@ -12,3 +13,10 @@ for year in root.findall('year'):
     print(year.tag, year.attrib)
 
 print(root[0][1].text)
+
+
+def export_xml(tree, path):
+    with open (path, 'wb') as f:
+        tree.write(f)
+
+export_xml(tree, os.path.join(os.getcwd(), 'writer.xml'))
