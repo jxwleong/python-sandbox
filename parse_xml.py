f76ff0b8bfb259d1584e1df5cfa31c750564c491
jxwleong <jason.xie.wei.leong@gmail.com>
2021-02-06 11:26:41 +0800

    Added functions to find and update element for simple xml file, will need to explore on more complicated xml file

--- parse_xml.py
+++ parse_xml.py
@@ -5,26 +5,29 @@ import xml.etree.ElementTree as ET
 
 ROOT_DIR = os.path.join(os.getcwd())
 
-print("Root DIR")
-print(ROOT_DIR)
-tree = ET.parse('sample.xml')
-root = tree.getroot()
-ET.dump(tree)
-for child in root:
-    print(child.tag, child.attrib)
-
-
-for year in root.findall('year'):
-    print(year.tag, year.attrib)
-
-print(root[0][1].text)
-
 
 def export_xml(tree, path):
     with open (path, 'wb') as f:
         tree.write(f)
 
 #export_xml(tree, os.path.join(ROOT_DIR, 'writer.xml'))
+
+def find_element(tree, tag):
+    return tree.find(tag)
+
+def update_element(element, value):
+    element.text = value
+
+print("Root DIR")
+print(ROOT_DIR)
+tree = ET.parse('sample.xml')
+root = tree.getroot()
+ET.dump(tree)
+
+version = find_element(tree, 'Version')
+update_element(version, '3.20.542.1')
+export_xml(tree, path=os.path.join(ROOT_DIR, "edited.xml"))
+
 
 def delete_file(path):
     if os.path.exists(path):
@@ -33,4 +36,4 @@ def delete_file(path):
         warnings.warn('Invalid file path!', Warning)
 #delete_file(os.path.join(ROOT_DIR, 'Edited.xml'))
 
-delete_file('C:/ABC')
+#delete_file('C:/ABC')
