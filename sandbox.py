8a8855c6aeee0097e9d815f402542a1d2b0ae1b3
jxwleong <jason.xie.wei.leong@gmail.com>
2021-02-07 12:43:10 +0800

    Added logging dict config with color using colorama

--- sandbox.py
+++ sandbox.py
@@ -1,9 +1,15 @@
 import logging
 import sys
 
-def test():
-    logger = logging.getLogger()
-    logger.info("This is info ")
+COLOR = {
+    "HEADER": "\033[95m",
+    "BLUE": "\033[94m",
+    "GREEN": "\033[92m",
+    "RED": "\033[91m",
+    "ENDC": "\033[0m",
+}
+logger = logging.getLogger()
+logger.info(COLOR["GREEN"], "This is info ")
 
 a_logger = logging.getLogger()
 
@@ -20,6 +26,16 @@ a_logger.addHandler(output_file_handler)
 a_logger.addHandler(stdout_handler)
 
 
-test()
+
+import colorama
+#colorama.init()
+start = "\033[1;31m"
+end = "\033[0;0m"
+print("File is: \033[1;31m '<placeholder>' + end")
+
+#import os
+#os.system("color")
 
 
+print('\u001b[31;1m')
+#print(COLOR["GREEN"], "Testing Green!!", COLOR["ENDC"])
