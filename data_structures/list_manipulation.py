92dbf4e377fca9ed3ef80896993ddbd698f7e688
jxwleong <jason.xie.wei.leong@gmail.com>
2021-02-08 19:54:13 +0800

    Unittest hang due to function call to append_self_if_single_list. After some trial and error (via comment), finally find out who cause the issue... Anyway rename some variable/function that got messed up during 'find and replace all' on previous commit. Will disable unittest, call the function and step debug it.

--- data_structures/list_manipulation.py
+++ data_structures/list_manipulation.py
@@ -12,29 +12,29 @@ def append_self_if_single_list(list_):
     return list_
 
 
-def get_combination_in_list(*numberlist): 
-    numberlist = append_self_if_single_list(*numberlist)
+def get_combination_in_list(*list_): 
+    list_ = append_self_if_single_list(*list_)
     combination_list = []
-    for item in list(itertools.product(*numberlist)):
+    for item in list(itertools.product(*list_)):
         combination_list.append(list(item))
     return combination_list
 
-# def list_to_string(*numberlist_s):
-#     strlist_ = []
-#     _str = ' '
-#     for element in list(numberlist_s):
-#         print(str(element))
-#         _str.join(str(element))
-#
-#     strlist_.append(_str)
-#     return strlist_
+def list_to_string(*list_):
+    str_list = []
+    str_ = ' '
+    for element in list(list_):
+        print(str(element))
+        str_.join(str(element))
+
+    str_list.append(str_)
+    return str_list
 
 def is_nested_list(list_):
     return any(isinstance(element, list) for element in list_)
 
 
-class Test_is_nestedlist_(unittest.TestCase):
-    def test_is_nestedlist_given_not_nested_list_expect_false(self):
+class Test_is_nested_list(unittest.TestCase):
+    def test_is_nested_list_given_not_nested_list_expect_false(self):
         self.assertEqual(False, is_nested_list([1, 2]))
         self.assertEqual(False, is_nested_list(['abc', '!@#$', '11aD']))
 
@@ -84,28 +84,28 @@ class Test_convert_list_to_string(unittest.TestCase):
         self.assertEqual('[low,high]', convert_list_to_string(['low', 'high']))
 
 
-class Test_append_self_if_single_list(unittest.TestCase):
-    def test_append_self_if_single_list_given_single_list_expect_duplicate_element_of_list(self):
-        self.assertEqual([1, 1], append_self_if_single_list([1]))
-        self.assertEqual([[1, 2], [1, 2]], append_self_if_single_list([1, 2]))
-        self.assertEqual([1, 2, 3, 4, 5], [1, 2, 3, 4, 5], append_self_if_single_list([1, 2, 3, 4, 5]))
-        self.assertEqual(['abcd'], ['abcd'], append_self_if_single_list(['abcd']))
-        self.assertEqual(['aaa', 'sss', 'ddd', 1], ['aaa', 'sss', 'ddd', 1], append_self_if_single_list(['aaa', 'sss', 'ddd', 1]))
+# class Test_append_self_if_single_list(unittest.TestCase):
+#     def test_append_self_if_single_list_given_single_list_expect_duplicate_element_of_list(self):
+#         self.assertEqual([1, 1], append_self_if_single_list([1]))
+#         self.assertEqual([[1, 2], [1, 2]], append_self_if_single_list([1, 2]))
+#         self.assertEqual([1, 2, 3, 4, 5], [1, 2, 3, 4, 5], append_self_if_single_list([1, 2, 3, 4, 5]))
+#         self.assertEqual(['abcd'], ['abcd'], append_self_if_single_list(['abcd']))
+#         self.assertEqual(['aaa', 'sss', 'ddd', 1], ['aaa', 'sss', 'ddd', 1], append_self_if_single_list(['aaa', 'sss', 'ddd', 1]))
 
-    # def test_append_self_if_single_list_given_non_single_list_expect_no_effect(self):
-        # self.assertEqual([[1], [2]], append_self_if_single_list([[1], [2]]))
-        # self.assertEqual([[1, 2], [3, 4]], append_self_if_single_list([[1, 2], [3, 4]]))
-        # self.assertEqual([[1, 2, 3, 4, 5], ['a', 'b']], append_self_if_single_list([[1, 2, 3, 4, 5], ['a', 'b']]))
-        # self.assertEqual([['abcd'], ['!@#$']], append_self_if_single_list([['abcd'], ['!@#$']]))
-        # self.assertEqual([['aaa', 'sss'], [2, 3], [1]], append_self_if_single_list([['aaa', 'sss'], [2, 3], [1]]))
+#     def test_append_self_if_single_list_given_non_single_list_expect_no_effect(self):
+#         self.assertEqual([[1], [2]], append_self_if_single_list([[1], [2]]))
+#         self.assertEqual([[1, 2], [3, 4]], append_self_if_single_list([[1, 2], [3, 4]]))
+#         self.assertEqual([[1, 2, 3, 4, 5], ['a', 'b']], append_self_if_single_list([[1, 2, 3, 4, 5], ['a', 'b']]))
+#         self.assertEqual([['abcd'], ['!@#$']], append_self_if_single_list([['abcd'], ['!@#$']]))
+#         self.assertEqual([['aaa', 'sss'], [2, 3], [1]], append_self_if_single_list([['aaa', 'sss'], [2, 3], [1]]))
 
 
-# class Test_get_combination_in_list(unittest.TestCase):
-#     def test_get_combination_in_list_given_singlelist_expect_get_all_the_possible_combination(self):
-#         self.assertEqual([[1, 1], [1, 2], [2, 1], [2, 2]], get_combination_in_list([1, 2]))
-#         self.assertEqual([[1, 1], [1, 2], [1, 3],   \
-#                           [2, 1], [2, 2], [2, 3],   \
-#                           [3, 1], [3, 2], [3, 3]], get_combination_in_list([1, 2, 3]))
+class Test_get_combination_in_list(unittest.TestCase):
+    def test_get_combination_in_list_given_singlelist_expect_get_all_the_possible_combination(self):
+        self.assertEqual([[1, 1], [1, 2], [2, 1], [2, 2]], get_combination_in_list([1, 2]))
+        self.assertEqual([[1, 1], [1, 2], [1, 3],   \
+                          [2, 1], [2, 2], [2, 3],   \
+                          [3, 1], [3, 2], [3, 3]], get_combination_in_list([1, 2, 3]))
 
 #     def test_get_combination_in_list_given_twolist_expect_get_all_the_possible_combination(self):
 #         self.assertEqual([[1, 3], [1, 4], [2, 3], [2, 4]], get_combination_in_list([1, 2], [3, 4]))
@@ -124,7 +124,7 @@ class Test_append_self_if_single_list(unittest.TestCase):
 #                           [2, 4, 5, 7], [2, 4, 5, 8], [2, 4, 6, 7], [2, 4, 6, 8]], get_combination_in_list([1, 2], [3, 4], [5, 6], [7, 8]))
 
 
-# if __name__ == '__main__':
-#     unittest.main()
+if __name__ == '__main__':
+    unittest.main()
 
 
