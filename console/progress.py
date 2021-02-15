a12ce3e9092850c757a8bc66bce5c344c0c1d79e
jxwleong <jason.xie.wei.leong@gmail.com>
2021-02-15 14:50:45 +0800

    Added testing script to print out progress bar with '■' Reference: https://stackoverflow.com/a/3160819

--- console/progress.py
+++ console/progress.py
@@ -1,7 +1,23 @@
 import time
 import sys
 
-for i in range(100):
-    i = i + 1
-    print("\rloading... %s%%" % i, end='')
-    time.sleep(.2)
+# for i in range(100):
+#     i = i + 1
+#     print("\rloading... %s%%" % i, end='')
+#     time.sleep(.2)
+
+toolbar_width = 40
+
+# setup toolbar
+# https://stackoverflow.com/a/3160819
+sys.stdout.write("[%s]" % (" " * toolbar_width))
+sys.stdout.flush()
+sys.stdout.write("\b" * (toolbar_width+1)) # return to start of line, after '['
+
+for i in range(toolbar_width):
+    time.sleep(0.1) # do real work here
+    # update the bar
+    print('■', end='', flush=True)
+    sys.stdout.flush()
+
+sys.stdout.write("]\n") # this ends the progress bar
