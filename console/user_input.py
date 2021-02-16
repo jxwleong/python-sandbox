a9a5ad299db014cd7d971b12bfa308c300cad0af
jxwleong <jason.xie.wei.leong@gmail.com>
2021-02-16 19:28:30 +0800

    Added user_input.py to test the autocompletion feature for prompt_toolkit
    The script must run in terminal, can't run in VSCode as it expect a 'real' terminal.
    
    Error
    prompt_toolkit.output.win32.NoConsoleScreenBufferError: No Windows console found. Are you running cmd.exe?

---
+++ console/user_input.py
@@ -0,0 +1,17 @@
+from prompt_toolkit import prompt
+from prompt_toolkit.completion import WordCompleter
+
+
+def user_input(message, debug_mode, completer=None):
+    if debug_mode:
+        return input(message)
+    else:
+        return prompt(message, completer=completer, complete_while_typing=True)
+
+completer = WordCompleter(['Hello', 'World'])
+text = user_input('Hello Debug: ', debug_mode=True)
+print('You said: %s Debug Mode' % text)
+
+text = user_input('Hello Prod: ', debug_mode=False, completer=completer)
+print('You said: %s Debug Prod' % text)
+
