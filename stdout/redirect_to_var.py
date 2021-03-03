b983ac8fdad64d387ff7a0b3c358cd4bfaf07821
jxwleong <jason.xie.wei.leong@gmail.com>
2021-03-03 18:55:33 +0800

    Python logging module will log to stderr stream by default, to redirect stdout to changed the stream to sys.stdout Looks like redirect stderr is trickier than I thought...

--- stdout/redirect_to_var.py
+++ stdout/redirect_to_var.py
@@ -1,9 +1,19 @@
 # https://stackoverflow.com/a/22434594
-
 import io
+import sys
 from contextlib import redirect_stdout
+import logging
 
 with io.StringIO() as buf, redirect_stdout(buf):
     print('redirected')
     output = buf.getvalue()
 print(output)
+
+
+
+
+with io.StringIO() as buf, redirect_stdout(buf):
+    logging.basicConfig(stream=sys.stdout,level=logging.DEBUG)
+    logging.debug('This will get logged')
+    output = buf.getvalue()
+print(f'Logger:Debug: {output}')
