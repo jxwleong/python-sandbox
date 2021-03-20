cc6c75543aa5a7f2518bfc2ca10f2660350ab9b7
jxwleong <jason.xie.wei.leong@gmail.com>
2021-03-20 18:58:08 +0800

    Added a simple function to check whether optinal argument is specified Assume optional argument without 'action', thus if not specified the value in Namespace is None

--- cmd_arguments/argparse_test.py
+++ cmd_arguments/argparse_test.py
@@ -1,10 +1,21 @@
 import argparse
+import time
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-k', '--keyvalue')
+parser.add_argument('-t', '--time', type=int)
 parser.add_argument('-v', '--verbose', action='store_true')
 parser.add_argument('-d', '--data')
 parser.add_argument('-f', '--file')
 
 args = parser.parse_args()
 print(args)
+
+def is_optional_argument_specified(argument):
+    """Assume argument w/o action"""
+    if argument is None:
+        return False
+    return True
+
+if is_optional_argument_specified(args.time):
+    print("Delay time: %d" % args.time)
+    time.sleep(args.time)
