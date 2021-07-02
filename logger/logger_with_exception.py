43e0b3efcc2f48fb5cbaf70665a935c932002b1a
jxwleong <jason.xie.wei.leong@gmail.com>
2021-07-02 18:53:00 +0800

    Update logger example with exception with function call

--- logger/logger_with_exception.py
+++ logger/logger_with_exception.py
@@ -3,7 +3,11 @@ import logger
 
 logger = logging.getLogger(__name__)
 
+
+def foo():
+    raise Exception("FOO!")
 try:
-    raise Exception("FAK!")
+    #raise Exception("FAK!")
+    foo()
 except:
     logger.exception("Exception!")
