6a2638285d38ff47891de24a2e33fa57db831905
jxwleong <jason.xie.wei.leong@gmail.com>
2021-07-27 18:59:13 +0800

    Added example to convert hex to decimal value

---
+++ math/int_conversion.py
@@ -0,0 +1,16 @@
+import unittest
+
+def hex2decimal(hex_: int):
+    return int(str(hex_), base=10)
+
+
+
+class Test_hex2decimal(unittest.TestCase):
+    def test_hex2decimal_expect_correct_value(self):
+        self.assertEqual(10, hex2decimal(0xa))
+        self.assertEqual(255, hex2decimal(0xff))
+
+
+
+if __name__ == '__main__':
+    unittest.main()
