8f44701f5863bb5982bdbe88a40d9e165d4ebd8e
jxwleong <jason.xie.wei.leong@gmail.com>
2021-09-26 16:11:02 +0800

    Added unittest for inheritance.py

--- class/inheritance.py
+++ class/inheritance.py
@@ -1,3 +1,5 @@
+import unittest
+
 class Person:
     """
     Base Class
@@ -20,3 +22,20 @@ engineer = Engineer("Master", "X", 20)
 print(f"Qualification: {engineer.qualification}")
 print(f"Name: {engineer.name}")
 print(f"Age: {engineer.age}")
+
+
+class Test_Class_Person(unittest.TestCase):
+    def test_instantiate_Person(self):
+        person = Person(name="Jay", age=3)
+        self.assertEqual(person.name, "Jay")
+        self.assertEqual(person.age, 3)
+
+class Test_Class_Person(unittest.TestCase):
+    def test_instantiate_Person(self):
+        engineer = Engineer("Master of Science", "X", 20)
+        self.assertEqual(engineer.qualification, "Master of Science")
+        self.assertEqual(engineer.name, "X")
+        self.assertEqual(engineer.age, 20)
+
+if __name__ == "__main__":
+    unittest.main()
