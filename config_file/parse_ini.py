71d41c52c2de87ef67b3440366f8bb891f5b459a
jxwleong <jason.xie.wei.leong@gmail.com>
2021-10-03 13:03:07 +0800

    Added function to get key-values pair under section def get_section_values(section: str):     _list = []     for key, value in config[section_map[section]].items():         _list.append(f"{key}={value}")     return _list

--- config_file/parse_ini.py
+++ config_file/parse_ini.py
@@ -1,18 +1,48 @@
 # https://docs.python.org/3/library/configparser.html
 import configparser
-
 import os
+import unittest
+
 ROOT_DIR = os.path.dirname(__file__)
 
+section_map = {
+    "settings_fault": "Settings Fault",
+    "settings_warning": "Settings Warning",
+    "settings_overrides": "Settings Overrides"
+
+
+}
+
 config = configparser.ConfigParser()
-a = config.read(os.path.join(ROOT_DIR, "myconfig.ini"))
+# To preserve the case
+# https://stackoverflow.com/a/1611877
+config.optionxform = str
+config.read(os.path.join(ROOT_DIR, "myconfig.ini"))
 
+def get_section_values(section: str):
+    """Extract key-values pair under section
 
-print(config.sections())
+    Args:
+        section (str): section name in the config file
 
-_list = []
-for key, value in config["Settings Fault"].items():
-    print(f"{key}={value}")
-    _list.append(f"{key}={value}")
+    Returns:
+        [list]: List of key-values pair 
+    """
+    _list = []
+    for key, value in config[section_map[section]].items():
+        _list.append(f"{key}={value}")
+    return _list
 
-print(_list)
+class Test_get_section_values(unittest.TestCase):
+    def test_get_section_values_settings_fault_expect_return_correct_list(self):
+        self.assertEqual(get_section_values("settings_fault"), ['foo=bar', 'user=jason'])
+
+    def test_get_section_values_settings_fault_expect_return_correct_list(self):
+        self.assertEqual(get_section_values("settings_warning"), ['warning=True'])
+
+    def test_get_section_values_settings_fault_expect_return_correct_list(self):
+        # Why .ini file is "Overrides=False" but function return "overrides=False"
+        self.assertEqual(get_section_values("settings_overrides"), ['Overrides=False'])
+
+if __name__ == "__main__":
+    unittest.main()
