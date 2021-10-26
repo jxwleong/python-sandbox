1ae6a1ba6a709431118101587cd756c6eb9977a7
jxwleong <jason.xie.wei.leong@gmail.com>
2021-10-26 19:52:25 +0800

    Added example to swap bytes and convert hex to ascii characters * Using processor string using hex from cpuid as example

---
+++ data_types/hex2ascii.py
@@ -0,0 +1,56 @@
+# Processor hex value get from http://www.jaist.ac.jp/iscenter-new/mpc/altix/altixdata/opt/intel/vtune/doc/users_guide/mergedProjects/analyzer_ec/mergedProjects/reference_olh/mergedProjects/instructions/instruct32_hh/vc46.htm
+
+
+cpuid_processor_string = {
+    "0x80000002": {"eax": "0x20202020", 
+                    "ebx": "0x20202020", 
+                    "ecx": "0x20202020",
+                    "edx": "0x6E492020"},
+    "0x80000003": {"eax": "0x286c6574", 
+                    "ebx": "0x50202952", 
+                    "ecx": "0x69746e65",
+                    "edx": "0x52286d75"},
+    "0x80000004": {"eax": "0x20342029", 
+                    "ebx": "0x20555043", 
+                    "ecx": "0x30303531",
+                    "edx": "0x007a484d"},
+}
+
+"""
+Ref: https://stackoverflow.com/a/27506699
+Swap little endian to big endian or vice versa
+
+msb             lsb
++------------------+
+| 12 | 34 | 56 | 78|
++------------------+
+
+lsb             msb
++------------------+
+| 78 | 56 | 34 | 12|
++------------------+
+"""
+def swap32(x):
+    return (((x << 24) & 0xFF000000) |
+            ((x <<  8) & 0x00FF0000) |
+            ((x >>  8) & 0x0000FF00) |
+            ((x >> 24) & 0x000000FF))
+
+
+def hex_str_to_ascii(hex_str, reverse=True):
+    if reverse is True:
+        hex_str = hex(swap32(int(hex_str, base=16)))
+    hex_ = hex_str[2:]  # Remove "0x"
+    hex_byte = bytes.fromhex(hex_)
+    ascii_byte = hex_byte.decode("ASCII")
+    return ascii_byte
+
+
+def get_processor_string():
+    str_ = ""
+    for cpuid in cpuid_processor_string:
+        for register, value in cpuid_processor_string[cpuid].items():
+            str_ += hex_str_to_ascii(value)
+    return str_
+
+print(get_processor_string())
