a943bfbf04d7ee679fa5e3483f2b66d3baaa4708
jxwleong <jason.xie.wei.leong@gmail.com>
2022-02-09 18:46:05 +0800

    Update example in abstract_class.py

--- design_pattern/abstract_class.py
+++ design_pattern/abstract_class.py
@@ -25,9 +25,21 @@ class CPU(Silicon):
         super().__init__(name)
 
 
+class PCH(Silicon):
+    def __init__(self, name=None):
+        super().__init__(name)
+    
 intel = CPU("Intel")
 print(intel, intel.name)
 
 amd = CPU("AMD")
-amd.name = "AMD"
+amd.name = "AMD"  # have to declare explicitly..
 print(amd, amd.name)
+
+
+intel_pch = PCH("Intel")
+print(intel_pch, intel_pch.name)
+
+amd_pch = PCH("AMD")
+amd_pch.name = "AMD"  # have to declare explicitly..
+print(amd_pch, amd_pch.name)
