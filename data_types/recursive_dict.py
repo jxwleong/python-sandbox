16c05035bdfc5d5e24ad5d52a74b99be53155c49
JLXW <jason.xie.wei.leong@gmail.com>
2022-11-01 21:02:15 +0800

    Added another way to walk the nested dictionary recursively

--- data_types/recursive_dict.py
+++ data_types/recursive_dict.py
@@ -21,3 +21,19 @@ json_data = {"test": [{"Tier1": [{"Tier1-Main-Title-1": [{"title": "main", "exam
                       {"Tier3": [{"Example1-Sub1": 44, "title": "TEST2"}]}]}
 
 print(get_data_value(json_data, 'title'))
+
+
+def dictionary_check(input):
+    """
+    First prints the final entry in the dictionary (most nested) and its key
+    Then prints the keys leading into this
+    * could be reversed to be more useful, I guess
+
+    src: https://gist.github.com/grandadmiral-thrawn/4a5531a1363863e74ded
+    """
+    for key,value in input.items():
+        if isinstance(value, dict):
+            dictionary_check(value)
+            print (key)
+        else:
+            print (key, value)
